0bf846f946d0a6a16a3055ee8ed87c59b4e678b6
xiecl <xiecl@yunk.ai>
2019-11-07 18:59:26 +0800

    update

--- src/main/java/com/github/justgame/mybatisplus/SpringMybatisPlusTestApplication.java
+++ src/main/java/com/github/justgame/mybatisplus/SpringMybatisPlusTestApplication.java
@@ -1,12 +1,19 @@
 package com.github.justgame.mybatisplus;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringMybatisPlusTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMybatisPlusTestApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> System.out.println("hello");
     }
 }
