364ffb0d3a62a7d10e51f0f58366b6d41d4018db
xiecongle <xiecongle@qudian.com>
2020-04-17 15:12:03 +0800

    add test duplicate key exception

--- src/test/java/com/github/justgame/mybatisplus/repository/UserRepositoryTest.java
+++ src/test/java/com/github/justgame/mybatisplus/repository/UserRepositoryTest.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.justgame.mybatisplus.model.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -45,5 +46,17 @@ class UserRepositoryTest {
     void testGetOne() {
         User user = userRepository.selectOne(Wrappers.query(new User().setName("test")));
         System.out.println(user);
+    }
+
+    @Test
+    void testDuplicateKey() {
+        User user = new User();
+        user.setName("xcl").setSex(1).setAge(18).setDescription("test duplicate key");
+        userRepository.insert(user);
+
+        User user2 = new User();
+        BeanUtils.copyProperties(user, user2);
+        userRepository.insert(user2);
+        // 出现DuplicateKeyException
     }
 }
